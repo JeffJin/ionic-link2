ab35f7e18ae91a263e3d6d2fe6747552232c0a9a
Jeff Jin <jeff@jeffjin.com>
2016-02-02 22:40:32 -0500

    pull to refresh added

--- link2/www/js/sections/sessions/sessions-controller.js
+++ link2/www/js/sections/sessions/sessions-controller.js
@@ -1,19 +1,27 @@
 angular.module('link2.controllers')
   .controller('SessionsCtrl', function($scope, $http, SessionService) {
-    //$scope.sessions = Session.query();
 
     $scope.reloadSessions = function(){
-      SessionService.getSessions().then(
+      if($scope.sessions && $scope.sessions.length){
+        $scope.$broadcast('scroll.refreshComplete');
+        $scope.sessions = _.reverse($scope.sessions);
+      }
+      else{
+        $scope.loadSessions().finally(function(){
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+      }
+    };
+
+    $scope.loadSessions = function(){
+      return SessionService.getSessions().then(
         function(result) {
-          $scope.sessions = _.reverse(result.data);
+          $scope.sessions = result.data;
         }, function(){
           $scope.sessions = [];
-        })
-        .finally(function() {
-          // Stop the ion-refresher from spinning
-          $scope.$broadcast('scroll.refreshComplete');
         });
     };
 
-    $scope.reloadSessions();
+    $scope.loadSessions();
+
   });
